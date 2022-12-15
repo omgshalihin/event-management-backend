6d55b8c3dc74dafbaec1c14ec3c33fdc1b7362fb
omgshalihin <shalihinsgp@gmail.com>
2022-12-15 02:29:24 +0100

    ready for dedployment

--- src/test/java/com/salt/hackday/HackdayApplicationTests.java
+++ src/test/java/com/salt/hackday/HackdayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.salt.hackday;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HackdayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.salt.hackday;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HackdayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
